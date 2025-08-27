d81e04a0f23b22585921110e68973f3e565b2796
李晓流 <2900629805@qq.com>
2025-08-27 13:53:41 +0800

    Update cmdrom.bat

--- Build/cmdrom.bat
+++ Build/cmdrom.bat
@@ -1,1 +1,1 @@
-EfiRom.exe -e pc-ultra-1-2-IntelGopDriver.efi IgdAssignmentDxe.efi PlatformGopPolicy.efi -f 0x8086 -i 0xffff -o ultra-1-2-qemu10.rom
+EfiRom.exe -e pc-ultra-1-2-IntelGopDriver.efi nb-ultra-155h.efi nb-ultra-255h.efi IgdAssignmentDxe.efi PlatformGopPolicy.efi -f 0x8086 -i 0xffff -o ultra-1-2-qemu10.rom
